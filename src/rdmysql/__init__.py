ce5586b6fa07fec3569ad60d974ec1b24ee9931c
Ryan Liu <azhai@126.com>
2016-12-30 19:33:25 +0800

    fix: Monthly.get_month_diff()

--- src/rdmysql/__init__.py
+++ src/rdmysql/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 from .database import Database
 from .table import Table, Monthly
--- src/rdmysql/table.py
+++ src/rdmysql/table.py
@@ -212,21 +212,25 @@ class Monthly(Table):
     calender = date.today()
     curr_has_subffix = True
     
+    def _add_month(self, num = 0):
+        return self.calender
+    
     def set_date(self, curr_date):
         self.calender = curr_date
         return self
     
-    def backward(self, monthes = 1):
-        for i in range(monthes): #小等于0时为空list
-            self.calender = self.calender.replace(day = 1)
-            self.calender -= timedelta(1)
+    def forward(self, monthes = 1):
+        total = self.calender.month + monthes - 1
+        ymd = dict(
+            year = self.calender.year + total / 12,
+            month = total % 12 + 1,
+            day = 1,
+        )
+        self.calender = self.calender.replace(**ymd)
         return self
     
-    def forward(self, monthes = 1):
-        for i in range(monthes): #小等于0时为空list
-            self.calender = self.calender.replace(day = 28)
-            self.calender += timedelta(4)
-        return self
+    def backward(self, monthes = 1):
+        return self.forward(0 - monthes)
         
     def get_month_diff(self, calender = None):
         calc_month_nth = lambda dt: (dt.year - 2000) * 12 + dt.month
@@ -236,7 +240,7 @@ class Monthly(Table):
             if not hasattr(self, '_curr_month_nth'):
                 self._curr_month_nth = calc_month_nth(date.today())
             month_nth = self._curr_month_nth
-        return month_nth - calc_month_nth(self.calender)
+        return calc_month_nth(self.calender) - month_nth
         
     def get_tablename(self):
         if not self.curr_has_subffix \
